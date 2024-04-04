d42f7873bd4b178c4ad592bff33b70a8d9ab3ba9
Almer June N. Ba-ad <120560008+almerjuneb@users.noreply.github.com>
2024-04-04 13:39:38 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,314 @@
-const mediaUrls = [
-  "../../assets/videos/1.mp4",
-  "../../assets/videos/2.mp4",
-  "../../assets/videos/3.mp4"
-];
+'use strict';
 
-let currentIndex = 0;
 
-let preloadedVideos = {};
-function preloadVideo(url) {
-  if (!preloadedVideos[url]) {
-    const video = document.createElement("video");
-    video.src = url;
-    video.type = "video/mp4";
-    video.preload = "auto";
-    video.style.display = "none";
-    document.body.appendChild(video);
-    preloadedVideos[url] = true;
-  }
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
 }
 
-function swapMedia() {
-  const mediaContainer = document.getElementById("media-container");
-  mediaContainer.innerHTML = "";
-  const url = mediaUrls[currentIndex];
-  const nextUrl = mediaUrls[(currentIndex + 1) % mediaUrls.length];
-  const isImg = /\.(jpg|jpeg|png|gif)$/i.test(url);
-  for (let i = 0; i < 4; i++) {
-    if (isImg) {
-      const img = document.createElement("img");
-      img.src = url;
-      mediaContainer.appendChild(img);
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+
+  testimonialsItem[i].addEventListener("click", function () {
+
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+    testimonialsModalFunc();
+
+  });
+
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
+
+
+
+// custom select variables
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () { elementToggleFunc(this); });
+
+// add event in all select items
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+
+  });
+}
+
+// filter variables
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+
+  for (let i = 0; i < filterItems.length; i++) {
+
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
     } else {
-      const video = document.createElement("video");
-      const source = document.createElement("source");
-      source.src = url;
-      source.type = "video/mp4";
-      video.autoplay = true;
-      video.loop = true;
-      i > 0 ? (video.style.opacity = currentOpacity) : "";
-      video.appendChild(source);
-      mediaContainer.appendChild(video);
-      preloadVideo(nextUrl);
+      filterItems[i].classList.remove("active");
     }
+
   }
-  currentIndex = (currentIndex + 1) % mediaUrls.length;
+
 }
 
-let ps5 = document.querySelector(".ps5");
-document.querySelector("#media-container").addEventListener("click", (e) => {
-  e.preventDefault();
-  if (ps5.classList.contains("on")) {
-    ps5.classList.remove("on");
-    currentIndex = 0;
+// add event in all filter button items for large screen
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
+
+}
+
+
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+
+  });
+}
+
+
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
+  });
+}
+
+let start = new Date().getTime();
+
+const originPosition = { x: 0, y: 0 };
+
+const last = {
+  starTimestamp: start,
+  starPosition: originPosition,
+  mousePosition: originPosition
+};
+
+const config = {
+  starAnimationDuration: 1500,
+  minimumTimeBetweenStars: 250,
+  minimumDistanceBetweenStars: 75,
+  glowDuration: 75,
+  maximumGlowPointSpacing: 10,
+  colors: ["249 146 253", "252 254 255"],
+  sizes: ["1.4rem", "1rem", "0.6rem"],
+  animations: ["fall-1", "fall-2", "fall-3"]
+};
+
+let count = 0;
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+  selectRandom = (items) => items[rand(0, items.length - 1)];
+
+const withUnit = (value, unit) => `${value}${unit}`,
+  px = (value) => withUnit(value, "px"),
+  ms = (value) => withUnit(value, "ms");
+
+const calcDistance = (a, b) => {
+  const diffX = b.x - a.x,
+    diffY = b.y - a.y;
+
+  return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+};
+
+const calcElapsedTime = (start, end) => end - start;
+
+const appendElement = (element) => document.body.appendChild(element),
+  removeElement = (element, delay) =>
+    setTimeout(() => document.body.removeChild(element), delay);
+
+const createStar = (position) => {
+  const star = document.createElement("span"),
+    color = selectRandom(config.colors);
+
+  star.className = "star fa-solid fa-sparkle";
+
+  star.style.left = px(position.x);
+  star.style.top = px(position.y);
+  star.style.fontSize = selectRandom(config.sizes);
+  star.style.color = `rgb(${color})`;
+  star.style.textShadow = `0px 0px 1.5rem rgb(${color} / 0.5)`;
+  star.style.animationName = config.animations[count++ % 3];
+  star.style.starAnimationDuration = ms(config.starAnimationDuration);
+
+  appendElement(star);
+
+  removeElement(star, config.starAnimationDuration);
+};
+
+const createGlowPoint = (position) => {
+  const glow = document.createElement("div");
+
+  glow.className = "glow-point";
+
+  glow.style.left = px(position.x);
+  glow.style.top = px(position.y);
+
+  appendElement(glow);
+
+  removeElement(glow, config.glowDuration);
+};
+
+const determinePointQuantity = (distance) =>
+  Math.max(Math.floor(distance / config.maximumGlowPointSpacing), 1);
+
+/* --  
+
+The following is an explanation for the "createGlow" function below:
+
+I didn't cover this in my video, but I ran into an issue where moving the mouse really quickly caused gaps in the glow effect. Kind of like this:
+
+*   *       *       *    *      *    🖱️
+
+instead of:
+
+*************************************🖱️
+
+To solve this I sort of "backfilled" some additional glow points by evenly spacing them in between the current point and the last one. I found this approach to be more visually pleasing than one glow point spanning the whole gap.
+
+The "quantity" of points is based on the config property "maximumGlowPointSpacing".
+
+My best explanation for why this is happening is due to the mousemove event only firing every so often. I also don't think this fix was totally necessary, but it annoyed me that it was happening so I took on the challenge of trying to fix it.
+
+-- */
+const createGlow = (last, current) => {
+  const distance = calcDistance(last, current),
+    quantity = determinePointQuantity(distance);
+
+  const dx = (current.x - last.x) / quantity,
+    dy = (current.y - last.y) / quantity;
+
+  Array.from(Array(quantity)).forEach((_, index) => {
+    const x = last.x + dx * index,
+      y = last.y + dy * index;
+
+    createGlowPoint({ x, y });
+  });
+};
+
+const updateLastStar = (position) => {
+  last.starTimestamp = new Date().getTime();
+
+  last.starPosition = position;
+};
+
+const updateLastMousePosition = (position) => (last.mousePosition = position);
+
+const adjustLastMousePosition = (position) => {
+  if (last.mousePosition.x === 0 && last.mousePosition.y === 0) {
+    last.mousePosition = position;
   }
-  currentIndex == 3 ? currentIndex++ : "";
-  swapMedia();
-});
-ps5.addEventListener("click", () => {
-  if (ps5.classList.contains("on")) {
-    ps5.classList.remove("on");
-    currentIndex = 4;
-    swapMedia();
-  } else {
-    ps5.classList.add("on");
-    currentIndex = 3;
-    swapMedia();
+};
+
+const handleOnMove = (e) => {
+  const mousePosition = { x: e.clientX, y: e.clientY };
+
+  adjustLastMousePosition(mousePosition);
+
+  const now = new Date().getTime(),
+    hasMovedFarEnough =
+      calcDistance(last.starPosition, mousePosition) >=
+      config.minimumDistanceBetweenStars,
+    hasBeenLongEnough =
+      calcElapsedTime(last.starTimestamp, now) > config.minimumTimeBetweenStars;
+
+  if (hasMovedFarEnough || hasBeenLongEnough) {
+    createStar(mousePosition);
+
+    updateLastStar(mousePosition);
   }
-});
 
-let currentOpacity = 1;
-document.querySelector("input#opacity").addEventListener("input", (e) => {
-  currentOpacity = e.target.value;
-  let bgTarget = document.querySelectorAll("video");
-  for (let i = 1; i < bgTarget.length; i++) {
-    bgTarget[i].style.opacity = currentOpacity;
-  }
-});
+  createGlow(last.mousePosition, mousePosition);
 
-const d = new Date();
-const hr = d.getHours() % 12 || 12;
-const min = d.getMinutes().toString().padStart(2, "0");
-document.querySelector(".display").innerHTML = `${hr}:${min}`;
+  updateLastMousePosition(mousePosition);
+};
 
+window.onmousemove = (e) => handleOnMove(e);
 
+window.ontouchmove = (e) => handleOnMove(e.touches[0]);
+
+document.body.onmouseleave = () => updateLastMousePosition(originPosition);
